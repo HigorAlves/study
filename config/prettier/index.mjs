dc8d9add2f8131148e4f2d308e3eacbf7da29128
Higor Alves <me@higoralves.dev>
2024-05-21 19:19:10 -0300

    chore: Remove redundant configs, scripts, components, and SVG files

---
+++ config/prettier/index.mjs
@@ -0,0 +1,18 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+export const config = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'always',
+	endOfLine: 'auto',
+	bracketSameLine: false
+}
+export default config
